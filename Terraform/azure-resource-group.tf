ec4a395c3b75cbb415c9b6918bb6066038559e67
Munaz Syed <mmsyed@irely.net>
2022-08-18 17:48:30 +0530

    basic commands

--- Terraform/azure-resource-group.tf
+++ Terraform/azure-resource-group.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "my_demo" {
   location = "eastus"
-  name = "my-demo-rg1"  
+  name = "my-demo"  
 }
